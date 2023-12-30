583a06beff88b783fb06e9ce13cfd40d5e162f12
AniketD23 <dasaniketk@gmail.com>
2023-12-30 18:26:30 -0500

    user input and search filtering for one website

--- whitelist-search-engine/routes.py
+++ whitelist-search-engine/routes.py
@@ -2,21 +2,32 @@ from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
+search_urls = []
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/add_search_url', methods=['POST'])
+def add_search_url():
+    if request.method == 'POST':
+        search_urls.append(request.form['search_url'])
+        return render_template('index.html')
+
+
 @app.route('/search', methods=['POST'])
 def search():
+    print(search_urls)
     if request.method == 'POST':
         search_query = request.form['query']
-        
         API_Key = open('API-Key').read()
         Search_Engine_ID = open('Search-Engine-ID').read()
         
         url = 'https://www.googleapis.com/customsearch/v1'
         params = {
+            'siteSearch': search_urls[0],
+            'siteSearchFilter': "i",
             'q': search_query,
             'key': API_Key,
             'cx': Search_Engine_ID
