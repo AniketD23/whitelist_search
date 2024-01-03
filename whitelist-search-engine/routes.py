c82df019299500294613fa3f3fff35aee6d07f0a
AniketD23 <dasaniketk@gmail.com>
2024-01-03 14:55:23 -0500

    added support for multiple websites

--- whitelist-search-engine/routes.py
+++ whitelist-search-engine/routes.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
-search_urls = []
+search_urls = {}
 
 
 @app.route('/')
@@ -12,36 +12,37 @@ def index():
 @app.route('/add_search_url', methods=['POST'])
 def add_search_url():
     if request.method == 'POST':
-        search_urls.append(request.form['search_url'])
+        search_urls[request.form['search_url']] = 0
         return render_template('index.html')
 
 
 @app.route('/search', methods=['POST'])
 def search():
-    print(search_urls)
     if request.method == 'POST':
         search_query = request.form['query']
         API_Key = open('API-Key').read()
         Search_Engine_ID = open('Search-Engine-ID').read()
         
         url = 'https://www.googleapis.com/customsearch/v1'
-        params = {
-            'siteSearch': search_urls[0],
-            'siteSearchFilter': "i",
-            'q': search_query,
-            'key': API_Key,
-            'cx': Search_Engine_ID
-        }
-        
-        responses = requests.get(url, params=params)
-        results = responses.json()
-        
-        if 'items' in results:
-            links = [item['link'] for item in results['items']]
-            return render_template('results.html', links=links)
+        for s_url in search_urls.keys():
+            params = {
+                'siteSearch': s_url,
+                'siteSearchFilter': "i",
+                'q': search_query,
+                'key': API_Key,
+                'cx': Search_Engine_ID
+            }
+
+            responses = requests.get(url, params=params)
+            results = responses.json()
+            
+            if 'items' in results:
+                search_urls[s_url] = [item['link'] for item in results['items']]
+                
+        return render_template('results.html', url_dict=search_urls)
         
         # Handle case where no items are found
-        return "No results found."
+        # return "No results found."
 
 if __name__ == '__main__':
     app.run(debug=True)
